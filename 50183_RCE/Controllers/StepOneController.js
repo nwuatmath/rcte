880641fd0b2262e77841e71988bc1a2ab5cda160
Nancy Wu <NWu@mathematica.net>
2016-09-26 09:53:58 -0400

    files added first time

---
+++ 50183_RCE/Controllers/StepOneController.js
@@ -0,0 +1,35 @@
+﻿var stepOneController = function (StepOne) {
+    var post = function (req, res) {
+        var step1 = new StepOne(req.body);
+       
+        step1.save(function (err) {
+            if (err)
+                console.log(err);
+            else
+                res.status(201).send(steps);
+
+
+        });
+        
+    }
+
+    var get = function (req, res) {
+
+        var query = {};
+
+       //fill userid and evalid for the query
+        StepOne.find(query, function (err, step1) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(step1);
+        });
+    }
+
+    return {
+        post: post,
+        get: get
+    }
+}
+
+module.exports = stepOneController;
---
+++ 50183_RCE/Controllers/wizardStepController.js
@@ -0,0 +1,30 @@
+﻿
+var WizardStep = require('../models/wizardStep');
+
+var wizardStepController = function () {
+    
+
+    var get = function (req, res) {
+
+       
+        var query = {};
+
+        //fill userid and evalid for the query
+        WizardStep.find(query, function (err, wizardStep) {
+            if (err) {
+                
+                res.status(500).send(err);
+            }
+            else {
+                
+                res.json(wizardStep);
+            }
+        });
+    }
+
+    return {
+        get: get
+    }
+}
+
+module.exports = wizardStepController;
